fc896f84b883d509a75a464b69cba86505d665e9
Krishna Nigalye <krishna.nigalye@gmail.com>
2024-01-01 17:34:56 +0530

    Create breadthFirstValues.js

---
+++ breadthFirstValues.js
@@ -0,0 +1,60 @@
+/*
+breadth first values
+====================
+Write a function, breadthFirstValues, that takes in the root of a binary tree. 
+The function should return an array containing all values of the tree in breadth-first order.
+*/
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+const breadthFirstValues = (root)=>{
+    if (root === null)
+        return [];
+    let queue = [root];
+    let arr = [];
+
+    while (queue.length > 0) {
+        let current = queue.shift();
+        arr.push(current.val);
+        if (current.left !== null)
+            queue.push(current.left);
+        if (current.right !== null)
+            queue.push(current.right);
+    }
+
+    return arr;
+}
+
+/*
+
+// test cases
+
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
+const e = new Node('e');
+const f = new Node('f');
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+
+breadthFirstValues(a);
+//    -> ['a', 'b', 'c', 'd', 'e', 'f']
+
+*/
